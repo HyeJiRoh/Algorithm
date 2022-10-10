4446a325f3acc2f6cd4f3e1b8db4c8e3be5373fd
HyeJi Roh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-11 03:00:16 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		
+		System.out.println(A/B);
+	}
+
+}

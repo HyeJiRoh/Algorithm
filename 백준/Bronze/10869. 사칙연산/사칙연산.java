6f42b3d51d918a78a9d126393b80b7cfe5016889
HyeJi Roh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-11 03:02:06 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+
+}

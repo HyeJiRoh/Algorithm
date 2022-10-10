e6c438ebc13a12d22d582a062ec28ebd1e0ff438
HyeJi Roh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-11 02:54:43 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}

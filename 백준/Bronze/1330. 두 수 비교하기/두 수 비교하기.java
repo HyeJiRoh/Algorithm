ff1aa677c2c3783ad11ca554806404279132c210
HyeJi Roh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-11 03:45:06 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A>B) {System.out.println(">");}
+		else if (A==B) {System.out.println("==");}
+		else System.out.println("<");
+	}
+}

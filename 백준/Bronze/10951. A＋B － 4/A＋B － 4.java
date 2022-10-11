ff9e0a5c71e2a896454e58764aa489aa9501abf1
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-12 00:51:12 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+	}
+}

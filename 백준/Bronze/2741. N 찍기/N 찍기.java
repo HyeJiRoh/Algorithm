2d93a8b6ba5d1dca5841ce09dc2709dddc3b7378
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-13 01:36:34 +0900

    [Bronze V] Title: N 찍기, Time: 1040 ms, Memory: 32600 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			System.out.println(i);
+		}
+	}
+}

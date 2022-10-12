5fb0abf8db435e4d97a88823e31285af768985b6
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-13 01:39:26 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String[] word = sc.next().split("");
+		
+		System.out.println(word.length);
+	}
+}

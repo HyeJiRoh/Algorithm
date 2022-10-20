cb1442436bdbb85c22cfeff1f1766036d143a9e2
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-20 22:18:57 +0900

    [Bronze IV] Title: R2, Time: 200 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int r1 = sc.nextInt();
+		int r2= 0;
+		int s = sc.nextInt();
+	
+		r2 = s*2 - r1;
+		
+		System.out.println(r2);
+	}
+}

ccece05a18c5db44942ea1e3552fe686e0b9d81f
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-20 20:55:49 +0900

    [Bronze IV] Title: 기찍 N, Time: 1052 ms, Memory: 33272 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		for(int i=input; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+}

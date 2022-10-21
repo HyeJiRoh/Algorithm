594be00b1c5fe9d3473d0b8e74c5b7b9bfa69aee
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2022-10-21 23:29:30 +0900

    [Bronze V] Title: 사파리월드, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextInt();
+		long m = sc.nextInt();
+		
+		System.out.println(Math.abs(n-m));
+	}
+}

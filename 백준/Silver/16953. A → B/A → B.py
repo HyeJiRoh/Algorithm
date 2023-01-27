79276dfa03279b3cf81c2cb3a6f886960a194d82
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2023-01-28 04:36:57 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -6,13 +6,12 @@ count = 1
 
 while b!=a :
     temp = b
-
+    count += 1
+    
     if b%2 == 0 :
         b = b//2
-        count += 1
     elif b%10 == 1 :
         b = b//10
-        count += 1
 
     if temp == b :
         print(-1)

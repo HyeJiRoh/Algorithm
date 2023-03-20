5856d872863af812418088a161117f674a81b2ba
HyeJiRoh <86008693+HyeJiRoh@users.noreply.github.com>
2023-03-20 22:02:38 +0900

    [Bronze II] Title: 피보나치 수, Time: 184 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0 for i in range(n+1)]
+
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])

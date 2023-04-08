407cfca25eddd7358958897fb6f767a446a12028
HyeJiRoh <shgpwl509@naver.com>
2023-04-08 22:57:07 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 1344 ms, Memory: 732860 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    key, value = map(int, input().split())
+    graph[key].append(value)
+    graph[value].append(key)
+
+result = [-1] * (n+1)
+
+def dfs(node):
+    graph[node].sort()
+
+    for idx in graph[node]:
+        if result[idx] == -1:
+            result[idx] = result[node] + 1
+            dfs(idx)
+
+result[r] = 0
+
+dfs(r)   
+print(*result[1:], sep = '\n') 

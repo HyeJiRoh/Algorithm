0fc644937d97292aa3dd670bd43d519f093afecf
HyeJiRoh <shgpwl509@gmail.com>
2023-04-20 17:17:32 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+result = []
+
+def dfs(start):
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+
+    for idx in range(start, n + 1):
+        if idx not in result:
+            result.append(idx)
+            dfs(idx)
+            result.pop()
+
+dfs(1)

29280593e6141dd86ac210f82a75017bbbcc6cdc
HyeJiRoh <shgpwl509@gmail.com>
2023-04-20 17:23:43 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -12,7 +12,7 @@ def dfs(start):
     for idx in range(start, n + 1):
         if idx not in result:
             result.append(idx)
-            dfs(idx)
+            dfs(idx+1)
             result.pop()
 
 dfs(1)

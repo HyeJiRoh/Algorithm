6abdfeff8538929e7c2057111bfcd5621e3147e5
HyeJiRoh <shgpwl509@gmail.com>
2023-04-23 18:42:09 +0900

    [Silver IV] Title: 수 찾기, Time: 248 ms, Memory: 139992 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,11 +1,30 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = set(map(int, input().split()))
+nrr = list(map(int, input().split()))
 
 m = int(input())
-brr = list(map(int, input().split()))
+mrr = list(map(int, input().split()))
 
-for i in brr :
-  if i in arr :
-    print(1)
-  else :
-    print(0)
+nrr.sort()
+
+for num in mrr:
+    start = 0
+    end = n - 1
+    
+    while start <= end:
+        mid = (start + end) // 2
+
+        if num == nrr[mid]:
+            print(1)
+            break
+
+        elif num > nrr[mid]:
+            start = mid + 1
+
+        else:
+            end = mid - 1
+
+    if start > end:
+        print(0)

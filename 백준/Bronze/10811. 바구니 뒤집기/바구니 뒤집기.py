9f7ae9d15cd8f6ed152a866cd42e960ba0651557
HyeJiRoh <shgpwl509@gmail.com>
2023-05-25 16:15:11 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+basket = [i for i in range(0, n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    temp = basket[a:b+1]
+    temp.reverse()
+    basket[a:b+1] = temp
+
+print(*basket[1:])

e6eb7f973b21441747d4aee8ae21b1f529dd7593
HyeJiRoh <shgpwl509@gmail.com>
2023-05-31 16:17:44 +0900

    [Silver II] Title: 수열의 합, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1024. 수열의 합/수열의 합.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+t = 0
+x = -1
+iter = 0
+for i in range(l, 101):
+    t = (i * i - i) / 2
+    if (n - t) % i == 0 and (n - t) // i >= 0:
+        x = (n - t) // i
+        iter = i
+        break
+if x == -1:
+    print(-1)
+else:
+    for i in range(iter):
+        print(int(x + i))

94b5ae6c08c9d347aa32ed1dc0c89ea5ff779c5e
HyeJiRoh <shgpwl509@gmail.com>
2023-08-04 22:48:24 +0900

    [Gold IV] Title: 연구소, Time: 3324 ms, Memory: 141544 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,56 @@
+import sys
+input = sys.stdin.readline
+
+n,m =  map(int, input().split())
+graph = []
+temp = [[0]*m for _ in range(n)]
+
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+result = 0
+
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and ny >= 0 and nx < n and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
+def score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+
+
+def dfs(count):
+    global result
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = graph[i][j]
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i,j)
+        result = max(result,score())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                count += 1
+                dfs(count)
+                graph[i][j] = 0
+                count -= 1
+dfs(0)
+print(result)

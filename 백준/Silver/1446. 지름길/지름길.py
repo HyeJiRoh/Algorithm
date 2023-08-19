723b9aaedc08a61208a421bfea39294aa4d41686
HyeJiRoh <shgpwl509@gmail.com>
2023-08-19 15:09:18 +0900

    [Silver I] Title: 지름길, Time: 192 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -0,0 +1,13 @@
+N, D = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+distance = [i for i in range(D + 1)]
+
+for i in range(D + 1):
+    if i > 0:
+        distance[i] = min(distance[i], distance[i - 1] + 1)
+
+    for s, e, d in arr:
+        if i == s and e <= D and distance[i] + d < distance[e]:
+            distance[e] = distance[i] + d
+
+print(distance[D])

8dbd81d0ddf11a6848dd72dda26f027c02396933
HyeJiRoh <shgpwl509@gmail.com>
2023-09-07 22:14:42 +0900

    [Silver IV] Title: 문자열, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+a, b = input().split()
+
+result = []
+
+for i in range(len(b) - len(a) + 1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            cnt += 1
+    result.append(cnt)
+
+print(min(result))

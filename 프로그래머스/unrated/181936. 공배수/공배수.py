7bb1e0cd906b0af0fd34e0d75185e8353c8d276f
HyeJiRoh <shgpwl509@gmail.com>
2023-09-25 16:29:21 +0900

    [unrated] Title: 공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,5 +1,3 @@
 def solution(number, n, m):
     answer = 0
-    if number % n == 0 and number % m == 0:
-        answer = 1
-    return answer
+    return 1 if (number % n == 0 and number % m == 0) else 0

eb14accc30dee6e1d4441d69e8b9445a0fc8414e
HyeJiRoh <shgpwl509@gmail.com>
2023-10-05 01:16:27 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    for num in combinations(nums, 3):
+        data = sum(num)
+        flag = True
+        
+        for i in range(2, data):
+            if data % i == 0:
+                flag = False
+                
+        if flag:
+            answer += 1
+            
+    return answer

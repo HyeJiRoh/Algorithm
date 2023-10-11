011b2d387e626d84233d932fb06df6579afe18a2
HyeJiRoh <shgpwl509@gmail.com>
2023-10-12 00:37:21 +0900

    [level 1] Title: 최소직사각형, Time: 3.31 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,12 +1,13 @@
 def solution(sizes):
     answer = 0
     left_max, right_max = 0, 0
-    for idx in range(len(sizes)):
-        if sizes[idx][0] < sizes[idx][1]:
-            sizes[idx][0], sizes[idx][1] = sizes[idx][1], sizes[idx][0]
-        if sizes[idx][0] > left_max:
-            left_max = sizes[idx][0]
-        if sizes[idx][1] > right_max:
-            right_max = sizes[idx][1]
-    answer = left_max * right_max
+    
+    for left, right in sizes:
+        if left < right:
+            left, right = right, left
+            
+        left_max = max(left_max, left)
+        right_max = max(right_max, right)
+    
+    answer = left_max*right_max
     return answer

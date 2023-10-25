87d547dce0006070621461f518b96596a4d2372e
HyeJiRoh <shgpwl509@gmail.com>
2023-10-26 01:00:37 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,5 @@
 def solution(n):
     answer = 0
-    data = str(bin(n))
     for num in range(n+1, 1000001):
-        num_data = str(bin(num))
-        if num_data.count('1') == data.count("1"):
+        if str(bin(num)).count('1') == str(bin(n)).count("1"):
             return num

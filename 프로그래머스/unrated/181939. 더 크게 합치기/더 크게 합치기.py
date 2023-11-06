b5950ba2ee99c99b3c81b09aa87878e1f8e74f93
HyeJiRoh <shgpwl509@gmail.com>
2023-11-07 00:49:10 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,11 +1,2 @@
 def solution(a, b):
-    answer = 0
-    
-    tmp_a = str(a) + str(b)
-    tmp_b = str(b) + str(a)
-    
-    if int(tmp_a) >= int(tmp_b):
-        answer = tmp_a
-    elif int(tmp_b) > int(tmp_a):
-        answer = tmp_b
-    return int(answer)
+    return int(max(str(a) + str(b), str(b) + str(a)))

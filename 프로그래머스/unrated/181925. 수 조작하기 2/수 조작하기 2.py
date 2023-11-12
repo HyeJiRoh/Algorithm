1acaf16cb8d8509b4fd7fbcaeff08cd8abd855ae
HyeJiRoh <shgpwl509@gmail.com>
2023-11-12 15:46:42 +0900

    [level unrated] Title: 수 조작하기 2, Time: 23.06 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,15 @@
+def solution(numLog):
+    answer = ""
+    start = numLog[0]
+    
+    for i in range(1, len(numLog)):
+        if numLog[i] == numLog[i-1] + 1:
+            answer += "w"
+        elif numLog[i] == numLog[i-1] - 1:
+            answer += "s"
+        elif numLog[i] == numLog[i-1] + 10:
+            answer += "d"
+        elif numLog[i] == numLog[i-1] - 10:
+            answer += "a"
+        
+    return answer

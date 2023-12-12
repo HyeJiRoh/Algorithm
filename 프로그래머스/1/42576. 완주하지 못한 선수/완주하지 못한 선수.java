45f139561bbc5067f98930ba91710e275f825d5c
HyeJiRoh <shgpwl509@gmail.com>
2023-12-12 23:34:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 298.39 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         Arrays.sort(participant);
         Arrays.sort(completion);
         
-        for(int i = 0; i < completion.length; i++){
-            if(!participant[i].equals(completion[i])){
+        for(int i = 0; i < completion.length; i++) {
+            if(participant[i].equals(completion[i])) {
+                continue;
+            } else {
                 return participant[i];
             }
         }
-        
         return participant[participant.length-1];
     }
 }

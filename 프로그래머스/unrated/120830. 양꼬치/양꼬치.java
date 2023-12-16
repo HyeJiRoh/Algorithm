b3091ca9f003aab43cb8e0b040a6e44791b72ee5
HyeJiRoh <shgpwl509@gmail.com>
2023-12-16 13:12:03 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        answer = n * 12000 + k * 2000 - (n / 10) * 2000;
-        return answer;
+        return n * 12000 + k * 2000 - (n / 10) * 2000;
     }
 }

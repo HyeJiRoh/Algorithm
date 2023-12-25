aae1289e725dd1c514bb20caf61d0bbb0d907344
HyeJiRoh <shgpwl509@gmail.com>
2023-12-25 11:28:32 +0900

    [Silver I] Title: 오르막 수, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [1] * 10
+
+for _ in range(1, n):
+    for i in range(1, 10):
+        dp[i] += dp[i - 1]
+
+print(sum(dp) % 10007)

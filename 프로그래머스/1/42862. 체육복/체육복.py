778f8f0526d62785b6d9c504b15d0e33f4651203
HyeJiRoh <shgpwl509@gmail.com>
2024-01-01 17:57:51 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -4,13 +4,13 @@ def solution(n, lost, reserve):
     
     for check in reserve[:]:
         if check in lost:
-            reserve.remove(check)
             lost.remove(check)
+            reserve.remove(check)
     
     for check in reserve:
         if check-1 in lost:
             lost.remove(check-1)
         elif check+1 in lost:
             lost.remove(check+1)
-    
-    return n-len(lost)
+            
+    return n - len(lost)

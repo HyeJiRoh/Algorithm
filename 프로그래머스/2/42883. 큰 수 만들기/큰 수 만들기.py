3d38ff8ace4e301486c3b3b3faff0ec4796aeb88
HyeJiRoh <shgpwl509@gmail.com>
2024-01-01 18:36:31 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,11 +1,12 @@
 def solution(number, k):
+    answer = ''
     stack = []
     for num in number:
-        while len(stack) != 0 and k > 0 and stack[-1] < num:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
             stack.pop()
             k -= 1
         stack.append(num)
-        
+    
     if k != 0:
         return number[:-k]
     else:

0df9433b54c386dbca8a5cdeb852052f7a184045
HyeJiRoh <shgpwl509@gmail.com>
2024-01-01 22:03:54 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,16 @@
-n = int(input())
-result = 0
+import sys
+input = sys.stdin.readline
 
-while n>=0 :
-    if n%5 == 0 :
-        result += (n//5)
-        print(result)
+n = int(input())
+cnt = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        cnt += (n//5)
+        print(cnt)
         break
-    else :
-        n-=3
-        result +=1
-else :
+    n -= 3
+    cnt += 1
+    
+else:
     print(-1)

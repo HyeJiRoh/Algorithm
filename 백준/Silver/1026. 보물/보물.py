a462f4f880cbe04350598a00caf1715dfbccacd2
HyeJiRoh <shgpwl509@gmail.com>
2024-01-01 22:51:48 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -2,14 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+arr = list(map(int, input().split()))
+brr = list(map(int, input().split()))
 
-arr = list(map(int, input().strip().split()))
-brr = list(map(int, input().strip().split()))
 arr.sort()
-sort_brr = sorted(brr, reverse=True)
-result = 0
+brr.sort(reverse=True)
 
-for i in range (n):
-    result += arr[i]*sort_brr[i]
+total = 0
 
-print(result)
+for i in range(n):
+    total += (arr[i] * brr[i])
+
+print(total)

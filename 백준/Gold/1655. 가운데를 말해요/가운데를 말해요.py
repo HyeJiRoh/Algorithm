1d24d585f928c20f4eac05c36983bf2aa7916243
HyeJiRoh <shgpwl509@gmail.com>
2024-01-03 22:35:23 +0900

    [Gold II] Title: 가운데를 말해요, Time: 368 ms, Memory: 124036 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -13,12 +13,11 @@ for _ in range(n):
         heapq.heappush(left_heap, -num)
     else:
         heapq.heappush(right_heap, num)
+    
+    if left_heap and right_heap and left_heap[0]*-1 > right_heap[0]:
+        max_value = heapq.heappop(left_heap)
+        min_value = heapq.heappop(right_heap)
+        heapq.heappush(left_heap, min_value*-1)
+        heapq.heappush(right_heap, max_value*-1)
         
-    if left_heap and right_heap and -left_heap[0] > right_heap[0]:
-        left_value = heapq.heappop(left_heap)
-        right_value = heapq.heappop(right_heap)
-        
-        heapq.heappush(left_heap, right_value * -1)
-        heapq.heappush(right_heap, left_value * -1)
-        
-    print(left_heap[0] * -1)
+    print(-left_heap[0])

3adbd1dc1347696d0a1d45814b062382f7d6465a
HyeJiRoh <shgpwl509@gmail.com>
2024-01-04 20:20:56 +0900

    [Gold III] Title: 줄 세우기, Time: 232 ms, Memory: 116296 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -4,20 +4,20 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-in_degree = [0] * (n+1)
+in_degree = [0] *(n+1)
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     in_degree[b] += 1
-    
-result = []
+
 q = []
+result = []
 
 for i in range(1, n+1):
     if in_degree[i] == 0:
         heapq.heappush(q, i)
-
+    
 while q:
     cur = heapq.heappop(q)
     result.append(cur)
@@ -26,5 +26,5 @@ while q:
         in_degree[g] -= 1
         if in_degree[g] == 0:
             heapq.heappush(q, g)
-
+            
 print(" ".join(map(str, result)))

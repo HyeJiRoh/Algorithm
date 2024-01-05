444d0e6dced87a0eada6a5fe747749368ced8e5a
HyeJiRoh <shgpwl509@gmail.com>
2024-01-05 15:50:19 +0900

    [Gold III] Title: 순회강연, Time: 184 ms, Memory: 112980 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.py
+++ 백준/Gold/2109. 순회강연/순회강연.py
@@ -4,18 +4,20 @@ input = sys.stdin.readline
 
 n = int(input())
 lectures = []
+visited = [0] * 10001
 
 for _ in range(n):
     p, d = map(int, input().split())
     lectures.append([p, d])
 
 lectures.sort(key=lambda x: x[1])
+
 q = []
 
 for pay, day in lectures:
     heapq.heappush(q, pay)
     
-    if len(q) > day:
+    if day < len(q):
         heapq.heappop(q)
 
 print(sum(q))

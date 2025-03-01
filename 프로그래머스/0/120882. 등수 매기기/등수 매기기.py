0a81ba77907b39fea6fdb1c8c1471cf481925cc0
HyeJiRoh <shgpwl509@gmail.com>
2025-03-01 13:11:51 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,14 @@
+def solution(score):
+    answer = []
+    tmp = []
+    result = []
+    for score_value in score:
+        total = sum(score_value)
+        answer.append(total)
+        tmp.append(total)
+    answer.sort(reverse=True)
+    
+    for t in tmp:
+        if t in answer:
+            result.append(answer.index(t)+1)
+    return result
